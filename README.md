31109333dc2671bbba4c726761eb896f0134aecf
Patrick Daether <public@pd-digital.de>
2024-08-14 21:22:23 +0200

    Init Repo

---
+++ README.md
@@ -0,0 +1,16 @@
+# My Linux dotfiles
+
+Managed with stow.
+
+Install everything with
+
+```bash
+sudo .apps.sh
+```
+
+Then link the dotfiles with
+
+```bash
+stow .
+```
+
---
+++ apps.sh
@@ -0,0 +1,19 @@
+# Replacement for cat with syntax highlighting
+# https://github.com/sharkdp/bat
+nala install bat -y
+# Replacenent for ls
+# Eza is not as good as exa, I don't know why it's not available in the repo anymore
+# Install via binary: https://the.exa.website/install/linux
+#nala install eza -y
+nala install htop -y
+nala install neofetch -y
+nala install nmap -y
+# Filter for shell https://github.com/peco/peco
+nala install peco -y
+# For symlinking the dotfiles: https://www.gnu.org/software/stow/
+nala install stow
+nala install tig -y
+# https://tldr.sh/
+nala install tldr -y
+nala install tree -y
+nala install xclip -y
