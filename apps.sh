61b03d011c0e898e1979334a83a2bbcd451e0392
Patrick Daether <public@pd-digital.de>
2025-11-22 14:52:01 +0100

    Add some packages

--- apps.sh
+++ apps.sh
@@ -4,7 +4,7 @@ nala install bat -y
 # Replacenent for ls
 # Eza is not as good as exa, I don't know why it's not available in the repo anymore
 # Install via binary: https://the.exa.website/install/linux
-#nala install eza -y
+nala install eza -y
 nala install htop -y
 nala install neofetch -y
 nala install nmap -y
@@ -17,3 +17,6 @@ nala install tig -y
 nala install tldr -y
 nala install tree -y
 nala install xclip -y
+nala install lazygit -y
+# For nice system monitoring:
+nala install conky-all -y
